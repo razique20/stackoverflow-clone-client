9848311d1d7f5a561f0c51f338e15a61e8fdce6c
Razique Mk <raziquemk2002@gmail.com>
2024-04-27 22:53:48 +0530

    main-bar done

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home/Home";
 import Auth from "./components/pages/Auth/Auth";
+import Questions from "./components/pages/Questions/Questions";
 
 const AllRoutes = () => {
   return (
@@ -10,6 +11,8 @@ const AllRoutes = () => {
       <Route path="/" element={<Home />} />
 
       <Route path="/Auth" element={<Auth />} />
+
+      <Route path="/Questions" element={<Questions />} />
     </Routes>
   );
 };
--- src/components/HomeMainbar/HomeMainbar.jsx
+++ src/components/HomeMainbar/HomeMainbar.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import './HomeMainbar.css'
+import {Link , useLocation } from 'react-router-dom'
+import QuestionList from './QuestionList'
 
 const HomeMainbar = () => {
+  var questionList = [
+    {
+      id:1,
+      votes:3,
+      noOfAnswers:2,
+      questionTitle:"what is a function",
+      questionBody: "It meant to be",
+      questionTags:["java","node js","react js","mongo db"],
+      userPosted:"mano",
+      askedOn:"jan 1"
+    },{
+      id:2,
+      votes:0,
+      noOfAnswers:0,
+      questionTitle:"what is a function",
+      questionBody: "It meant to be",
+      questionTags:["java","node js","react js","mongo db"],
+      userPosted:"mano",
+      askedOn:"jan 1"
+    },{
+      id:3,
+      votes:1,
+      noOfAnswers:0,
+      questionTitle:"what is a function",
+      questionBody: "It meant to be",
+      questionTags:["java","javascript","R","mongo db"],
+      userPosted:"mano",
+      askedOn:"jan 1"
+    }
+  ]
+  const location = useLocation()
   return (
-    <div>HomeMainbar</div>
+    <div className='main-bar'>
+      <div className='main-bar-header'> 
+    {
+      location.pathname === '/' ? <h1>Top Questions</h1> : <h1>All Questions</h1>
+    }
+    <Link to='/AskQuestion' className='ask-btn'>Ask Question</Link>
+      </div>
+
+      <div>
+        {
+          questionList === null ? 
+          <h1>Loading....</h1> : 
+          <>
+          <p>{questionList.length} questions</p>
+         
+         
+          <QuestionList questionList={questionList} />
+         
+          </>
+        }
+      </div>
+    </div>
   )
 }
 
